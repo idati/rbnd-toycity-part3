9715e59a6f862be3b31968585aca944a67178aeb
idati <specht.hartweger@hotmail.de>
2016-04-26 06:30:57 +0200

    add in product.rb => function find_item and add in errors.rb => class DuplicateProductError

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,43 @@
 # product.rb
 
 class Product
+  attr_reader :title,:price,:stock
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+ 
+    if !find_item
+      add_to_product
+    else
+      raise DuplicateProductError, "#{@title} already exists."
+    end
+ 
   end
 
+  def find_item
+  @@products.each{|x| if x.title==@title
+                        return true
+                      end}
+  return false
+  end
+  
+  #def in_stock
+  #  if 
+  #end
+  
+  #def find_by_title
+  #  
+  #end
+  
   def self.all
     @@products
+  end
+  
+  private
+  def add_to_product
+        @@products << self
   end
 end
